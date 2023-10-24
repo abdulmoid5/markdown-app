0a4d171e608aa20c34c77ba8609057aa08a44930
Abdul Moied <abdul@theohealth.com>
2023-10-24 17:16:50 +0100

    inital code added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { marked } from "marked"; // Import the 'marked' function from the 'marked' library
+import "./App.css";
+
+marked.setOptions({
+  breaks: true,
+});
+
+const initialMarkdown = `
+# Heading 1
+## Heading 2
+[Link](https://www.example.com)
+\`inline code\`
+\`\`\`
+// code block
+function greet(name) {
+  return 'Hello, ' + name + '!';
+}
+\`\`\`
+- List item 1
+- List item 2
+
+> Blockquote
+
+![Image](https://via.placeholder.com/150)
+**Bold Text**
+`;
 
 function App() {
+  const [markdown, setMarkdown] = useState(initialMarkdown);
+
+  const handleEditorChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="editor-container">
+        <textarea
+          id="editor"
+          value={markdown}
+          onChange={handleEditorChange}
+          placeholder="Enter your markdown here..."
+        />
+      </div>
+      <div className="preview-container">
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        />
+      </div>
     </div>
   );
 }
